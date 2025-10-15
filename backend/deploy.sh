76121ee02b7ad8a73f07f5e961c1d75c5cab6ac1
Bruno Pereira <bruno.pereira@aswti.com.br>
2025-10-14 21:29:37 -0300

    Inclusão dos logs dos clientes no terminal

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eu
 APP_VERSION=0.0.1
-IMAGE=github.com/brunospereira/tcc:$APP_VERSION
+IMAGE=backend:$APP_VERSION
 
 echo "Efetuando Docker Build da versão $APP_VERSION"
 docker build -t $IMAGE --build-arg APP_VERSION=$APP_VERSION .
